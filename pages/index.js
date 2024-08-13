d888744447f34ea841ae57bcad0e4ca32846a082
Ricardo Medeiros <ricmed@gmail.com>
2024-08-13 22:45:15 +0000

    frase corrigida

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Para Nágela</h1><p>Da manga rosa quero o gosto e o sumo \n Melão maduro, sapoti, juá \n Jaboticaba, teu olhar noturno \n     Beijo travoso de umbu, cajá \n     Pele macia, é carne de caju \n     Saliva doce, doce mel, mel de uruçu \n     Linda morena, fruta de vez temporana \n     Caldo de cana caiana \n     Vou te desfrutar \n     Linda morena, fruta de vez temporana \n     Caldo de cana caiana \n     Vem me desfrutar</p>
+    return <h1>Para Nágela <p>Da manga rosa quero o gosto e o sumo <br></br> Melão maduro, sapoti, juá <br></br> Jaboticaba, teu olhar noturno <br></br>     Beijo <strike>travoso</strike> gostoso de umbu, cajá <br></br>     Pele macia, é carne de caju <br></br>     Saliva doce, doce mel, mel de uruçu <br></br>     Linda morena, fruta de vez temporana <br></br>    Caldo de cana caiana <br></br>     Vou te desfrutar <br></br>     Linda morena, fruta de vez temporana <br></br>     Caldo de cana caiana <br></br>     Vem me desfrutar</p></h1>
 }
 
 export default Home;
