cd9e095cf291081f1790106043b097da229b815f
Ricardo Medeiros <ricmed@gmail.com>
2024-08-19 20:24:38 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Para Nágela <p>Da manga rosa quero o gosto e o sumo <br></br> Melão maduro, sapoti, juá <br></br> Jaboticaba, teu olhar noturno <br></br>     Beijo <strike>travoso</strike> gostoso de umbu, cajá <br></br>     Pele macia, é carne de caju <br></br>     Saliva doce, doce mel, mel de uruçu <br></br>     Linda morena, fruta de vez temporana <br></br>    Caldo de cana caiana <br></br>     Vou te desfrutar <br></br>     Linda morena, fruta de vez temporana <br></br>     Caldo de cana caiana <br></br>     Vem me desfrutar</p></h1>
+  return (
+    <h1>
+      Para Nágela{" "}
+      <p>
+        Da manga rosa quero o gosto e o sumo <br></br> Melão maduro, sapoti, juá{" "}
+        <br></br> Jaboticaba, teu olhar noturno <br></br> Beijo{" "}
+        <strike>travoso</strike> gostoso de umbu, cajá <br></br> Pele macia, é
+        carne de caju <br></br> Saliva doce, doce mel, mel de uruçu <br></br>{" "}
+        Linda morena, fruta de vez temporana <br></br> Caldo de cana caiana{" "}
+        <br></br> Vou te desfrutar <br></br> Linda morena, fruta de vez
+        temporana <br></br> Caldo de cana caiana <br></br> Vem me desfrutar
+      </p>
+    </h1>
+  );
 }
 
 export default Home;
